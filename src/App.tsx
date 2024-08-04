a55854ba6a7e21703073881e01f4b0afcb740bf8
mk <mk.eng1119@gmail.com>
2024-08-05 06:01:12 +0900

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import { Router } from "./components/router/Routes";
+
 function App() {
-  return (
-    <h1 className="text-3x text-red-400 font-bold underline">Hello world!</h1>
-  );
+  return <Router />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 
-import { Global } from "@emotion/react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
-import globalStyle from "./globalStyles";
-
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Global styles={globalStyle} />
     <App />
   </React.StrictMode>,
 );
